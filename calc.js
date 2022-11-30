4d2a71dc2f3eeda987e93f2950b4b5f519a19bdb
Georgina Edwards <118028272+Notunique1@users.noreply.github.com>
2022-11-29 19:04:18 -0500

    first commit

--- calc.js
+++ calc.js
@@ -1,4 +1,6 @@
-const readline = require('readline');
+const readline = require('Calculator'); { console.log (calculator)}
+
+
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -12,7 +14,9 @@ reader.question("What would you like to calculate?", function(input){
 	
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
-	const num2 = Number(tokens[2]);
+	const num2 = Number(tokens[2]); {console.log }
+
+
 
 
 	// This line closes the connection to the command line interface.
